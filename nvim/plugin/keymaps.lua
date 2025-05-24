1886baf4ea68f0a660370dd502bdc9b1061ff47f
Jermeiah S <owner@arouzing.xyz>
2025-05-24 16:18:50 -0400

    added write leader

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -8,6 +8,9 @@ local fn = vim.fn
 local keymap = vim.keymap
 local diagnostic = vim.diagnostic
 
+-- Save file
+vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, silent = true })
+
 -- Yank from current position till end of current line
 keymap.set('n', 'Y', 'y$', { silent = true, desc = '[Y]ank to end of line' })
 
