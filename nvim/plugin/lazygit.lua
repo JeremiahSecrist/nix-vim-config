afbcf38f6394fdf07d2ed3aaa2c35385a166e616
Jeremiah S <owner@arouzing.xyz>
2025-07-28 21:24:24 -0400

    improve lazygit

--- nvim/plugin/lazygit.lua
+++ nvim/plugin/lazygit.lua
@@ -1,5 +1,6 @@
 local Terminal = require("toggleterm.terminal").Terminal
 
+-- Create a new terminal for Lazygit
 local lazygit = Terminal:new({
   cmd = "lazygit",
   hidden = true,
@@ -8,15 +9,22 @@ local lazygit = Terminal:new({
     border = "double",
   },
   on_open = function(term)
-    -- Start in insert mode
+    -- Start in insert mode when the terminal opens
     vim.cmd("startinsert!")
 
-    -- Disable <Esc> in terminal mode (normal keymap)
+    -- Disable <Esc> in terminal mode to prevent exiting insert mode
     vim.api.nvim_buf_set_keymap(term.bufnr, 't', '<Esc>', '', { noremap = true, silent = true })
+
+    -- Safely remove any existing <C-l> mapping in terminal mode
+    pcall(vim.api.nvim_buf_del_keymap, term.bufnr, 't', '<C-l>')
   end,
 })
 
+-- Toggle function for Lazygit
 function Lazygit_toggle()
   lazygit:toggle()
 end
-vim.keymap.set("n", "<leader>gg", "<cmd>lua Lazygit_toggle()<CR>", {noremap = true, silent = true})
+
+-- Keymap to toggle Lazygit with <leader>gg
+vim.keymap.set("n", "<leader>gg", "<cmd>lua Lazygit_toggle()<CR>", { noremap = true, silent = true })
+
